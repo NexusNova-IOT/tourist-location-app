06eaed588d146ef41472ef99f94f4776bbbcbdb5
Ivanovich0705 <u201919381@upc.edu.pe>
2023-11-15 23:23:12 -0500

    feat: override callback for endpoints request

--- app/src/main/java/pe/upc/tourist_location/presentation/MainActivity.kt
+++ app/src/main/java/pe/upc/tourist_location/presentation/MainActivity.kt
@@ -8,6 +8,7 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,11 +22,12 @@ import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import pe.upc.tourist_location.location.model.TouristLocation
 import pe.upc.tourist_location.location.services.LocationClient
+import pe.upc.tourist_location.ui.LocationScreen
+import pe.upc.tourist_location.ui.NoPermissionScreen
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.HttpException
 import retrofit2.Response
-import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     private val locationManager: LocationManager by lazy {
@@ -37,6 +39,7 @@ class MainActivity : ComponentActivity() {
     private var hasRequestedPermissions by mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
         val token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImE2YzYzNTNmMmEzZWMxMjg2NTA1MzBkMTVmNmM0Y2Y0NTcxYTQ1NTciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGlmZXRyYXZlbC1hcHAiLCJhdWQiOiJsaWZldHJhdmVsLWFwcCIsImF1dGhfdGltZSI6MTcwMDEwNTEzMiwidXNlcl9pZCI6IjJpRmNqeTJVaHhOdkJET1pNclFkMlZEb0lzeTIiLCJzdWIiOiIyaUZjankyVWh4TnZCRE9aTXJRZDJWRG9Jc3kyIiwiaWF0IjoxNzAwMTA1MTMyLCJleHAiOjE3MDAxMDg3MzIsImVtYWlsIjoiaW90QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJpb3RAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.pmAnl6682bgS7Me3J86GS79BG4Nd9bu0N872_GzsnVF7uDtoomr_FuSMmZzmdO5mLxa12sL8PbC2KhNjVuCO2cJELisX4cv37SLh7KJGK2W68P1N5mD0q6w_b-zO-7jNTcVSU8pCfS00XRVCKZxSOFRy4T-eZNcstSBHcFwzz-Q5E6URK3KIWp2tyxtE8eBm1KbZEt0niQ9FY48QcypQpq_Yk8P7tdpLTJOqpaSkEGU0agtVj-_UgtrwM62cutzFP5ycvMjnKhG0RTo38d7OSXKDe322l8SzCvta72mDSZbiuPH-JYERIA9BC4bej3yrL57KPfdV3GzRMl0MxWCetQ"
         super.onCreate(savedInstanceState)
         thread {
@@ -74,7 +77,8 @@ class MainActivity : ComponentActivity() {
                 }
             })
         }
-        /*
+         */
+        super.onCreate(savedInstanceState)
         setContent {
             if (hasLocationPermission()) {
                 LocationProvider {
@@ -87,7 +91,6 @@ class MainActivity : ComponentActivity() {
                 NoPermissionScreen(requestLocationPermission = ::requestLocationPermission)
             }
         }
-        */
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -163,12 +166,51 @@ class MainActivity : ComponentActivity() {
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             val lastLocation = locationResult.lastLocation
-            currentLocation = lastLocation
+            if (currentLocation == null || hasLocationChanged(currentLocation, lastLocation)) {
+                currentLocation = lastLocation
+                sendLocationToBackend(currentLocation)
+            }
         }
     }
     private val locationRequest = LocationRequest.create().apply {
         interval = 10000
         fastestInterval = 5000
         priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+    private fun hasLocationChanged(oldLocation: Location?, newLocation: Location): Boolean {
+        return oldLocation?.latitude != newLocation.latitude || oldLocation.longitude != newLocation.longitude
+    }
+    private fun sendLocationToBackend(location: Location?) {
+        location?.let {
+            val token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImE2YzYzNTNmMmEzZWMxMjg2NTA1MzBkMTVmNmM0Y2Y0NTcxYTQ1NTciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGlmZXRyYXZlbC1hcHAiLCJhdWQiOiJsaWZldHJhdmVsLWFwcCIsImF1dGhfdGltZSI6MTcwMDEwNTEzMiwidXNlcl9pZCI6IjJpRmNqeTJVaHhOdkJET1pNclFkMlZEb0lzeTIiLCJzdWIiOiIyaUZjankyVWh4TnZCRE9aTXJRZDJWRG9Jc3kyIiwiaWF0IjoxNzAwMTA1MTMyLCJleHAiOjE3MDAxMDg3MzIsImVtYWlsIjoiaW90QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJpb3RAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.pmAnl6682bgS7Me3J86GS79BG4Nd9bu0N872_GzsnVF7uDtoomr_FuSMmZzmdO5mLxa12sL8PbC2KhNjVuCO2cJELisX4cv37SLh7KJGK2W68P1N5mD0q6w_b-zO-7jNTcVSU8pCfS00XRVCKZxSOFRy4T-eZNcstSBHcFwzz-Q5E6URK3KIWp2tyxtE8eBm1KbZEt0niQ9FY48QcypQpq_Yk8P7tdpLTJOqpaSkEGU0agtVj-_UgtrwM62cutzFP5ycvMjnKhG0RTo38d7OSXKDe322l8SzCvta72mDSZbiuPH-JYERIA9BC4bej3yrL57KPfdV3GzRMl0MxWCetQ"
+            val locationData = TouristLocation(latitude = it.latitude, longitude = it.longitude)
+
+            val locationService = LocationClient.create()
+            val call = locationService.updateLocation(token, locationData)
+
+            call.enqueue(object : Callback<Void> {
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    if (response.isSuccessful) {
+                        if (response.code() == 200) {
+                            println("Successful operation.")
+                        } else {
+                            println("Call error: ${response.code()}")
+                        }
+                    } else {
+                        println("Call error: ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    println("Call error: ${t.message}")
+                    println(t.stackTraceToString())
+
+                    if (t is HttpException) {
+                        val errorBody = t.response()?.errorBody()?.string()
+                        println("Error body: $errorBody")
+                    }
+                }
+            })
+        }
     }
 }
